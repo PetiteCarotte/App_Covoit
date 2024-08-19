091cfcad507bab89f4cc304880d62cfed720350e
Caroline Petit <caroline.petit-4@etu.univ-tours.fr>
2024-08-19 13:05:21 +0200

    Dossier Documentation

---
+++ DocumentationCovoit/ScriptsBDD/bases_militaires.sql
@@ -0,0 +1,5 @@
+INSERT INTO public.base_militaires (nom_de_la_base, nom_de_la_commune, acronyme, code_postal, created_at, updated_at)
+VALUES 
+    ('14eme BSMat Nouâtre', 'Nouâtre', '14eme BSMat Nouatre', '37800', current_timestamp, current_timestamp),
+    ('Base Aérienne 927 de Cinq Mars la Pile', 'Cinq Mars la Pile', 'B.A. 927 Cinq Mars la Pile', '37130', current_timestamp, current_timestamp),
+    ('Base Aérienne 705 de Tours', 'Tours', 'B.A. 705 Tours', '37100', current_timestamp, current_timestamp);
